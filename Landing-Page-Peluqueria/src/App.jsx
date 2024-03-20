37a03674f3c125e394f232afeeae59287c73472a
Yessica <yessilpd91@outlook.com>
2024-03-20 19:42:05 -0300

    agregando carrusel de imagenes

--- Landing-Page-Peluqueria/src/App.jsx
+++ Landing-Page-Peluqueria/src/App.jsx
@@ -3,11 +3,12 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Form from "./components/Form/Form";
-
+import Carrusel from "./components/Form/carrusel"
 function App() {
   return (
     <div>
       <Form />
+      <Carrusel />
     </div>
   );
 }
