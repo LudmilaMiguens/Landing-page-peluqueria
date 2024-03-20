4133650763a5c5da34ee6519a09caecd39082ca7
marcelotenaglia <tenaco23@gmail.com>
2024-03-20 20:09:53 -0300

    carousel 1

--- Landing-Page-Peluqueria/src/App.jsx
+++ Landing-Page-Peluqueria/src/App.jsx
@@ -6,9 +6,6 @@ import Form from "./components/Form/Form";
 import Carrusel from "./components/Carrusel/Carrusel";
 import BackGround from "./components/bck-pic/Background";
 import BackgroundImg from "./images/barberia.jpg";
-import foto1 from "./images/foto1.jpg";
-import foto2 from "./images/foto2.jpg";
-import foto3 from "./images/foto3.jpg";
 
 function App() {
   return (
