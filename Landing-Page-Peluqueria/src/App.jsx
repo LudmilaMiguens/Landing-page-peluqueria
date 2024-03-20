68f4d12070033ab05d67f513e58636101218f136
marcelotenaglia <tenaco23@gmail.com>
2024-03-20 19:50:02 -0300

    fondo de pagina

--- Landing-Page-Peluqueria/src/App.jsx
+++ Landing-Page-Peluqueria/src/App.jsx
@@ -3,10 +3,13 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Form from "./components/Form/Form";
+import BackGround from "./components/bck-pic/Background";
+import BackgroundImg from "./images/barberia.jpg";
 
 function App() {
   return (
     <div>
+      {/* <BackGround img={BackgroundImg} /> */}
       <Form />
     </div>
   );
