7f9e29d6643827cc9e96fc83fd201c19e8f37a4a
marcelotenaglia <tenaco23@gmail.com>
2024-03-20 20:05:35 -0300

    cambios en carousel

--- Landing-Page-Peluqueria/src/App.jsx
+++ Landing-Page-Peluqueria/src/App.jsx
@@ -3,13 +3,13 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Form from "./components/Form/Form";
-<<<<<<< HEAD
+import Carrusel from "./components/Carrusel/Carrusel";
 import BackGround from "./components/bck-pic/Background";
 import BackgroundImg from "./images/barberia.jpg";
+import foto1 from "./images/foto1.jpg";
+import foto2 from "./images/foto2.jpg";
+import foto3 from "./images/foto3.jpg";
 
-=======
-import Carrusel from "./components/Form/carrusel"
->>>>>>> 37a03674f3c125e394f232afeeae59287c73472a
 function App() {
   return (
     <div>
